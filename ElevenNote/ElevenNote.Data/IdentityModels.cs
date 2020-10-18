40bde06225da9599a265af50675d9436eb865515
scollier90 <shaynecollier90@gmail.com>
2020-10-18 14:55:41 -0400

    Updated connecton string

--- ElevenNote/ElevenNote.Data/IdentityModels.cs
+++ ElevenNote/ElevenNote.Data/IdentityModels.cs
@@ -1,8 +1,13 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web.Http.Validation;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using static ElevenNote.Data.ApplicationDbContext.IdentityUserLoginConfiguration;
 
 namespace ElevenNote.Data
 {
@@ -28,6 +33,36 @@ namespace ElevenNote.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove < PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+
+        public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+        {
+            public IdentityUserLoginConfiguration()
+            {
+                HasKey(iul => iul.UserId);
+            }
+
+            public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+            {
+                public IdentityUserRoleConfiguration()
+                {
+                    HasKey(iur => iur.UserId);
+                }
+            }
         }
     }
 }
