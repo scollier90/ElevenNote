8ca1be979ade549b045bff2f205bb91d1c7e5ebc
scollier90 <shaynecollier90@gmail.com>
2020-10-18 16:17:19 -0400

    Added NoteService and NoteCreate classes

---
+++ ElevenNote/ElevenNote.Services/NoteService.cs
@@ -0,0 +1,58 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userID;
+        public NoteService(Guid userID)
+        {
+            _userID = userID;
+        }
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerID = _userID,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Notes
+                        .Where(e => e.OwnerID == _userID)
+                        .Select(
+                            e =>
+                                new NoteListItem
+                                {
+                                    NoteID = e.NoteID,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
